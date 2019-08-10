2a351fbe65dfe46b18c21048ef69937b774a459f
Brandon Herlong <brandon.herlong@gmail.com>
2019-08-09 23:16:08 -0500

    automation works now

--- test/Test1.py
+++ test/Test1.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(r"C:\Users\blher\MyProjects\FirstSeleniumTest\drivers\chromedriver.exe")
 # driver = webdriver.Firefox()
@@ -9,7 +10,8 @@ driver.set_page_load_timeout(10)
 driver.get("http://google.com")
 # name of Google Search bar is "q"
 driver.find_element_by_name("q").send_keys("Lebron James")
+
 # name of Google Search button is "btnK"
-driver.find_element_by_name("btnK").click()
+driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
 time.sleep(4)
 driver.quit()
